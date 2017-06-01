5014e70d681c562b09038955d9ac71c2dcf3b774
theinlwin <finderbar2016@gmail.com>
2017-06-01 17:31:29 +0630

    squard-string

--- squard-string.js
+++ squard-string.js
@@ -1,9 +1,3 @@
-function vertMirror(strng) {
-    return strng.split('\n').map((value) => Array.proptotype.reduce.call(value, (result, ch) => ch + result, ''));
-}
-function horMirror(strng) {
-    return strng.reverse();
-}
-function oper(fct, s) {
-   return fct(s);
-}
+const vertMirror = (str) => str.split('\n').map((val) => val.split('').reverse().join('')).join('\n');
+const horMirror = (str) => str.split('\n').reverse().join('\n');
+const oper = (fct, s) => fct(s);
